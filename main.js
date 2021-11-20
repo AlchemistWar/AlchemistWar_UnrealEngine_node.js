3fe05872f8b8a731a5ab2ee43455a4b3c7d319b7
idea2547 <53974956+idea2547@users.noreply.github.com>
2021-11-20 18:30:03 +0700

    Include return public address of user to Unreal engine

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-require('dotenv').config()
+
 const Moralis = require('moralis/node');
 const ipc = require('ipc-event-emitter').default(process); //ipc-event-emitter use to send and receive data between node.js and unreal engine
-const serverUrl = process.env.MORALIS_SERVER_URL;
-const appId = process.env.MORALIS_APP_ID;
+const serverUrl = 'https://yss2n1a1jfia.usemoralis.com:2053/server';
+const appId = 'wgXFG92adB9cgJGPModdvDnnfOVJcnmpDCA6wHEo';
 
 Moralis.start({ serverUrl, appId });
 
 
 console.log("Moralis_Start");
 
+
+getUserId = async() => {
+    const query = new Moralis.Query("PolygonBalance");
+    console.log("1pass");
+    const userId = await query.find();
+    console.log(userId);
+}
 
 //Function Login to Moralis
 Login = async (name,email,password) => {
@@ -17,8 +24,10 @@ Login = async (name,email,password) => {
     console.log("pass");
     
     try{
-        const user = await Moralis.User.logIn(email, password);
-        ipc.emit('SuccessLogin', 'Pass');//bind function to call event in unreal engine after success
+        await Moralis.User.logIn(email, password).then(function (user) {
+            accounts = user.get('accounts')/* AA */
+        })
+        ipc.emit('SuccessLogin', accounts);//bind function to call event in unreal engine after success
         console.log("Successfully Log in");
     } catch (error) {
         // Show the error message somewhere and let the user try again.
